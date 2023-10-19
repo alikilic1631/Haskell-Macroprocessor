e92d1d7e9d431d40195075f566c16607ec66dfbd
Ali Kilic <ak5323@ash30.doc.ic.ac.uk>
2023-10-19 17:57:14 +0100

    last attempt

--- src/MP.hs
+++ src/MP.hs
@@ -18,7 +18,8 @@ returning all the values that match with that key.
 > lookUp "A" [("A", 8), ("B", 9), ("C", 5), ("A", 7)] == [8, 7]
 -}
 lookUp :: String -> [(String, a)] -> [a]
-lookUp = undefined
+lookUp y [] = []
+lookUp y xs = [b | (a,b) <- xs , a == y]
 
 {-|
 This function will break up a string with some given separator
@@ -28,15 +29,25 @@ each "word" and the words themselves.
 splitText :: [Char] -- ^ the separators to split on
           -> String -- ^ the string to split
           -> ([Char], [String])
-splitText = undefined
-
+splitText k [] = ([] , [""])
+splitText k (c:cs)
+  | elem c k  = (c:seps, "":word:words) 
+  | otherwise = (seps , (c:word):words)
+  where 
+    (seps, word:words) = splitText k cs 
+   
+ 
 {-|
 This function interleaves the characters from the first argument
 list with the strings in the second argument. The second list must
 be non-empty.
 -}
 combine :: [Char] -> [String] -> [String]
-combine = undefined
+combine [] []         = []
+combine [] (k:ks)     = k:combine [] ks 
+combine (c:cs) []     = [c]:combine cs [] 
+combine (c:cs) (k:ks) = k:[c]:combine cs ks  
+
 
 {-|
 This function takes a list of lines and splits each line to
@@ -45,9 +56,13 @@ extract a list of keyword-definition pairs.
 > getKeywordDefs ["$x Define x", "$y 55"] == [("$x", "Define x"), ("$y", "55")]
 -}
 getKeywordDefs :: [String] -> KeywordDefs
-getKeywordDefs = undefined
+getKeywordDefs []       = [] 
+getKeywordDefs (str:strs) = (word, words_new):getKeywordDefs strs 
+  where
+    (_:seps,word:words) = splitText [' '] str
+    words_new             = concat(combine seps words)
 
-{-|
+{-|  
 This function takes the contents of two files, one containing
 a template and the other the definitions for each keyword
 found within the template. It extracts the keyword-definition
@@ -60,8 +75,17 @@ as a result.
 expand :: FileContents -- ^ the template file contents
        -> FileContents -- ^ the info file contents
        -> FileContents
-expand = undefined
+expand t i = concat (combine s [replaceWord x def | x <- w])
+  where 
+    (s,w) = splitText separators t
+    ni    = snd(splitText "\n" i)
+    def   = getKeywordDefs ni
 
 -- You may wish to uncomment and implement this helper function
 -- when implementing expand
--- replaceWord :: String -> KeywordDefs -> String
+    replaceWord :: String -> KeywordDefs -> String
+    replaceWord k@('$':_) def = head (lookUp k def)
+    replaceWord k def         = k
+
+
+
